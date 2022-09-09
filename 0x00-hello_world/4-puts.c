ab2fd185bca68a072141f5b17a7bd428e02b0c01
BethelOrie <bethel_peacee@yahoo.com>
2022-09-08 18:37:44 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,5 @@
  * Return: Always O (Success)
  */
 int main(void)
-{ 
- puts("Programming is like building a multilingual puzzle\n");
- return (O); 
+{ puts("Programming is like building a multilingual puzzle\n"); return (O); 
 }
