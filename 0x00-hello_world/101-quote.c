d2f0bb2b2d28c47e8ebb8f4b732a2af30acf2997
BethelOrie <bethel_peacee@yahoo.com>
2022-09-12 15:11:31 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,13 +4,13 @@
  *
  * main - prints exactly and that piece of art is useful
  *
- * Return: Always 1.
+ * Return: Always 0.
  */
 int main(void)
 {
 	write(2,
-		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
 		59);
-	
-	return (1);
+		
+		return (1);
 }
