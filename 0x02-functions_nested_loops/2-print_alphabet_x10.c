193710b0ed5f9628503110062053fa641d2a2fd1
BethelOrie <bethel_peacee@yahoo.com>
2022-09-14 15:19:50 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,9 +4,9 @@
  * add - Adds two integers.
  * @num1: The first integer to be added.
  * @num2: The second integer to be added.
- * Return: The riesult of the addition.
+ * Return: The result of the addition.
  */
-int main(int num1, int num2)
+int add(int num1, int num2)
 {
 	
 	return (num1 + num2);
