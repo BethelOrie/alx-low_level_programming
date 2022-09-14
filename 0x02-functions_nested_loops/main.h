f754c4e4bd43329810a8b5c2709f0fb980dfc4e9
BethelOrie <bethel_peacee@yahoo.com>
2022-09-14 13:47:23 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,4 @@
-#ifindef HOLBERTON_H_INCLUDED
+#ifndef HOLBERTON_H_INCLUDED
 #define HOLBERTON_H_INCLUDED
 
 int _putchar(char);
@@ -9,7 +9,7 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
-void jack_bauer(void);
+void jack_bauer(void); 
 void times_table(void);
 int add (int a, int b);
 void print_to_98(int n);
@@ -17,4 +17,3 @@ void print_times_table(int n);
 void num(int r, int n);
 
 #endif
-
