3e3a5b6c85d30908fbf92b084976652b3678ad47
BethelOrie <bethel_peacee@yahoo.com>
2022-09-15 03:06:49 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,1 +1,25 @@
+#include <unistd.h>
+#include <time.h>
 
+#include <stdio.h>
+/**
+ * main - Prints a random number and states whether it is positive, negative, or zero.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n;
+
+
+	if (n > 0)
+		printf("%d is positive\n", 98);
+
+	else if (n < 0)
+		printf("%d is negative\n", 0);
+
+	else
+		printf("%d is zero\n", -98);
+
+	return (0);
+}
