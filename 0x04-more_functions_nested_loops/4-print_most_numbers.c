99472b36e88e8dcf4cf5d40ef19966d66b389432
BethelOrie <bethel_peacee@yahoo.com>
2022-09-17 00:17:31 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,7 +3,7 @@
 /**
  * print_most_numbers - a function that prints the numbers, from 0 to 9
  *
- * Return: 0-9, excluding 2 and 4 followed by newline
+ * Return: 0-9, excluding 2 and 4 followed by a new line
  */
 void print_most_numbers(void)
 {
