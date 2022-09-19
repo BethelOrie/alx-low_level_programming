b241e8323702c0bc6ebe0f4b6d3463e2159c947c
BethelOrie <bethel_peacee@yahoo.com>
2022-09-19 16:08:46 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - puts half
+ * str: string parameter
+ */
+void puts_half(char *str)
+{
+	int X, Y, i;
+
+	X = strlen(str);
+	if (X % 2 == 1)
+		Y = X / 2 + 1;
+	else
+		Y = X / 2;
+	for (i = Y; i < X; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
