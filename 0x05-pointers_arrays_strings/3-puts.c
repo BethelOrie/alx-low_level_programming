001550472af570bce544e3fb193d3d79f594b563
BethelOrie <bethel_peacee@yahoo.com>
2022-09-19 13:58:56 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts - this is the function that puts
+ * @str: a parameter to _puts function
+ */
+void _puts(char *str)
+{
+	for (; *str != '\0'; s++)
+	{
+		_putchar(*str);
+	}
+}
