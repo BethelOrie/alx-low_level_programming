14956c91c3189dcce74c11494963026844c7156e
BethelOrie <bethel_peacee@yahoo.com>
2022-09-19 14:33:58 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,14 +5,12 @@
  *  print_rev - printing a string in reverse
  * @s: the string to be printed in reverse
  */
+
 void print_rev(char *s)
 {
 	int i, n;
-
-	s = "hello"
-
-
-		n = strlen(s);
+	
+	n = strlen(s);
 	for (i = n - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
