7f525f4c33dc19b4c7e70ef204304f014a7db6b0
BethelOrie <bethel_peacee@yahoo.com>
2022-09-20 03:21:57 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,19 +1,19 @@
 #include "main.h"
 #include <string.h>
 #include <stdio.h>
-
 /**
- * int _atoi - function that converts a string to an integer
+ * _atoi - function returns the integer representation of the string
+ * @s: a pointer to a string to be converted to an integer
  * Return: Always 0.
  */
 int _atoi(char *s)
 {
 	int value;
-	char str[20];
+	char s[20];
 
-	strcpy(str, "123");
+	strcpy(s, "123");
 
-	value = _atoi(str);
+	value = _atoi(s);
 	printf("String value = %s, Int value = %d\n", str, value);
 
 	return (0);
