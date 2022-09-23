8d0bab551f38a3a1ae190a219a129e76bdf4589d
BethelOrie <bethel_peacee@yahoo.com>
2022-09-22 18:08:04 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -4,16 +4,15 @@
 
 /**
  * _atoi - function returns the integer representation of the string
- *@ *s: function takes as input the string that is to be converted to integer
+ *@*str: function takes as input the string that is to be converted to integer
  * Return: Always 0.
  */
-int _atoi(char *s)
+int _atoi(char *str)
 {
 	int i;
-	char *s;
 
-	s = " -9885";
-	i = _atoi(s);/* i = - 9885*/
-	
-	printf("i = %d\n",i);
+	str = " -9885";
+	i = _atoi(str);/* i = - 9885*/
+
+	printf("i = %d\n", i);
 }
