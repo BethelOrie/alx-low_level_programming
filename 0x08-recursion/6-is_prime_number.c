083ecea3fbb88cbea71c23ade625a836796151d6
BethelOrie <bethel_peacee@yahoo.com>
2022-09-29 15:25:12 -0700

    6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+*int is_prime_number -evaluates prime or not
+*@n: Number
+*Return: return 1 prime - return 0 otherwise
+*/
+int is_prime_number(int n)
+{
+
+int iterator;
+
+iterator = 2;
+
+/* only greater than 2*/
+if (n < 2)
+{
+return (0);
+}
+
+if (n == 2)
+{
+return (1);
+}
+return ('\n');
+}
