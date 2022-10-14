675b17a141891395a6631238841f2b9365799654
BethelOrie <bethel_peacee@yahoo.com>
2022-10-14 14:39:07 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,17 +1,47 @@
 #include "dog.h"
 
 /**
- *free_dog - free memory
- *@: array
+ *new_dog - creat a new dog
+ *@name: first member
+ *@age: 2nd member
+ *@owner: 3rd member
+ *
+ *Return: NULL if  Fail
  *Description: free memory to struct
  */
 
-void free_dog(dog_t *d)
+dog_t *new_dog(char *name, float age, char *owner)
 {
-	if (d)
+	int i, lenN, lenO;
+	struct dog *n_dog = NULL;
+
+	lenN = 0;
+	while (name[lenN] != '\0')
+		lenN++;
+
+	lenO = 0;
+	while (owner[lenO] != '\0')
+		lenO++;
+
+	n_dog = malloc(sizeof(struct dog));
+	if (n_dog == NULL)
 	{
-		free(d->name);
-		free(d->owner);
-		free(d);
+		free(n_dog);
+		return (NULL);
 	}
+	n_dog->name = malloc(lenO + 1);
+	if (n_dog->owner == NULL)
+	{
+		free(n_dog->name);
+		free(n_dog->owner);
+		free(n_dog);
+
+		return (NULL);
+	}
+	for (i = 0; i <= lenN; i++)
+		n_dog->name[i] = owner[i];
+	n_dog->age = age;
+
+	return (n_dog);
 }
+
