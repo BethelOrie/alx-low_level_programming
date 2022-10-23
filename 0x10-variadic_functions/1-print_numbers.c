a3ff1b26e595757647b5495118e7f2b808ce7fce
BethelOrie <bethel_peacee@yahoo.com>
2022-10-23 15:17:27 -0700

    -print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "variadic_functions.h"
 #include <stdarg.h>
+
 /**
  *print_numbers - check the code for Holberton School students.
  *@n: number of arguments
@@ -10,20 +11,18 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list numbers;
+	va_list nums;
 	unsigned int index;
 
-	va_start(numbers, n);
+	va_start(nums, n);
 
 		for (index = 0; index < n; index++)
 		{
-			printf("%d", va_arg(numbers, int));
+			printf("%d", va_arg(nums, int));
 
 			if (index != (n - 1) && separator != NULL)
 				printf("%s", separator);
 		}
 
 	printf("\n");
-
-	va_end(numbers);
 }
