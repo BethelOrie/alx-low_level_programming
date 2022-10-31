28daf1abd3f03229bf6b8c2e21acaeb3d26cca88
BethelOrie <bethel_peacee@yahoo.com>
2022-10-31 14:05:35 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,6 +9,7 @@
  * Description: Create a function that creates a file.
  * Return: 1 on success, -1 on failure
  */
+
 int create_file(const char *filename, char *text_content)
 {
 int i = 0, file;
@@ -18,7 +19,6 @@ return (-1);
 
 if (text_content == NULL)
 text_content = "";
-
 while (text_content[i] != '\0')
 {
 i++;
